2c88d8507a903bfb0e7804146b2b2fd3277ea74d
zohayrslileh <zohayrslileh@gmail.com>
2026-08-31 00:04:12 +0100

    Release Settings 0.1.5

--- phresh.config.ts
+++ phresh.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
     identity: "settings",
     name: "Settings",
     description: "Configure PhreshOS.",
-    version: "0.1.4",
+    version: "0.1.5",
     icon: "icon.png",
     categories: ["System"],
     keywords: ["settings", "appearance", "theme"],
