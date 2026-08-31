8348680da43f28a1f9bb80d294476e493ef76af3
zohayrslileh <zohayrslileh@gmail.com>
2026-08-31 02:17:13 +0100

    Align Program release metadata

--- phresh.config.ts
+++ phresh.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
     identity: "settings",
     name: "Settings",
     description: "Configure PhreshOS.",
-    version: "0.1.5",
+    version: "0.1.6",
     icon: "icon.png",
     categories: ["System"],
     keywords: ["settings", "appearance", "theme"],
