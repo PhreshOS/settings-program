7034d12aec00277c3692b3ef5100d41ebb69ea46
zohayrslileh <zohayrslileh@gmail.com>
2026-09-01 23:10:08 +0100

    Bundle reliable Server IPC

--- phresh.config.ts
+++ phresh.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
     identity: "settings",
     name: "Settings",
     description: "Configure PhreshOS.",
-    version: "0.1.8",
+    version: "0.1.9",
     icon: "icon.png",
     categories: ["System"],
     keywords: ["settings", "appearance", "theme"],
