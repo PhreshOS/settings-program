12c60c4679fdd1a2ad7dcfcdfb529ffdc4acbcba
zohayrslileh <zohayrslileh@gmail.com>
2026-09-01 22:23:20 +0100

    Synchronize Settings with System contracts

--- phresh.config.ts
+++ phresh.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
     identity: "settings",
     name: "Settings",
     description: "Configure PhreshOS.",
-    version: "0.1.7",
+    version: "0.1.8",
     icon: "icon.png",
     categories: ["System"],
     keywords: ["settings", "appearance", "theme"],
