336c55411599ca98bcaff6bffad0b3828fd52861
zohayrslileh <zohayrslileh@gmail.com>
2026-09-05 00:38:10 +0100

    Release 0.1.12 with synchronized SDKs

--- phresh.config.ts
+++ phresh.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
     identity: "settings",
     name: "Settings",
     description: "Configure PhreshOS.",
-    version: "0.1.11",
+    version: "0.1.12",
     icon: "icon.png",
     categories: ["System"],
     keywords: ["settings", "appearance", "theme"],
